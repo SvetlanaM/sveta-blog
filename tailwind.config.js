a639a47107b04d91e277882b44cbeeed66d6a6b0
SvetlanaM <svetlana@kompilator.cz>
2022-06-18 19:20:22 +0200

    Add basic tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      helveticaNeue: ["Helvetica Neue", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#000000",
+          100: "#212529",
+        },
+        gray: {
+          DEFAULT: "#6c757d",
+          100: "#f8f9fA",
+          200: "#dfdfdf",
+        },
+        blue: {
+          DEFAULT: "#007bff",
+          100: "#2b7efb",
+        },
+      },
+      borderColor: (theme) => ({
+        gray: theme("colors.gray"),
+      }),
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+  ],
+  corePlugins: {
+    outline: false,
+  },
+};
